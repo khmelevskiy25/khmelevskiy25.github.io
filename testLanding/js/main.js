d438a0fd682ed32972c4ed3be60dc5266d11dc2f
Khmelevskiy25 <Parkour22@bigmir.net>
2019-11-22 19:41:36 +0200

    testWork

---
+++ testLanding/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+   $('a.btn_lang').click(function(){
+        $('ul.lang_menu').toggleClass('active');
+   });
+   $('a.btn_lang').click(function(){
+    $(this).toggleClass('rotate');
+});
+});
