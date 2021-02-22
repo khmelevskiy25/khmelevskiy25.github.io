9b16c74cf7d53057019eba78fdd4b96846825c80
Khmelevskiy25 <Parkour22@bigmir.net>
2021-02-22 19:44:46 +0200

    change menu, change images, change color

--- site/js/main.js
+++ site/js/main.js
@@ -1,3 +1,18 @@
 jQuery(document).ready( function($) {
+    //comment slick
     $('.partners_slider').slick();
+
+    // //search
+    // $("#myInput").on("keyup", function() {
+    //     var value = $(this).val().toLowerCase();
+    //     $("#accordion .btn-link, #accordion .product-title").filter(function() {
+    //     $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    //     });
+    // });
+    //scroll
+    $("[name^='#']").click(function(){
+        var _href = $(this).attr("name");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+});
 });
