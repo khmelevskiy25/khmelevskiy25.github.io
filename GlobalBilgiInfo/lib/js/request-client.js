397c463015694b678fc483fcfa3af35f20d212b9
Igor <42932719+khmelevskiy25@users.noreply.github.com>
2023-09-24 14:27:17 +0000

    first commit

---
+++ GlobalBilgiInfo/lib/js/request-client.js
@@ -0,0 +1,70 @@
+async function getPosts()
+{
+    let res = await fetch('https://www.specposhiv.kiev.ua/api/posts');
+    let posts = await res.json();
+
+    posts.forEach((post) => {
+      if(post.post_type == "Шорткати")
+      {
+        document.querySelector('.accordion-shortcut').innerHTML += `
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+                    <b>${post.post_title}</b>
+                </button>
+            </h2>
+            <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show">
+                <div class="accordion-body"><strong>${post.post_body}</strong></div>
+            </div>
+        </div>
+        `
+      }
+      else if(post.post_type == "Відповіді на запитання")
+      {
+        document.querySelector('.accordion-answersquestions').innerHTML += `
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+                    <b>${post.post_title}</b>
+                </button>
+            </h2>
+            <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show">
+                <div class="accordion-body"><strong>${post.post_body}</strong></div>
+            </div>
+        </div>
+        `
+      }
+      else if(post.post_type == "Корисні посилання")
+      {
+        document.querySelector('.accordion-usefullinks').innerHTML += `
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+                    <b>${post.post_title}</b>
+                </button>
+            </h2>
+            <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show">
+                <div class="accordion-body"><strong>${post.post_body}</strong></div>
+            </div>
+        </div>
+        `
+      }
+      else if(post.post_type == "Загальна інформація")
+      {
+        document.querySelector('.accordion-generalinformation').innerHTML += `
+        <div class="accordion-item">
+            <h2 class="accordion-header">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+                    <b>${post.post_title}</b>
+                </button>
+            </h2>
+            <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show">
+                <div class="accordion-body"><strong>${post.post_body}</strong></div>
+            </div>
+        </div>
+        `
+      }
+    })
+}
+getPosts();
+
