0f4848951a2f908b6ce6d55a2a1da8db0b11248b
Igor <42932719+khmelevskiy25@users.noreply.github.com>
2023-09-26 12:15:42 +0000

    ver 1.1

--- GlobalBilgiInfo/lib/js/main.js
+++ GlobalBilgiInfo/lib/js/main.js
@@ -1,75 +1,84 @@
 $('.menu-btn').on('click', function(e){
-    e.preventDefault();
-    $('.menu').toggleClass('menu_active');
-    $('.content').toggleClass('content_active');
+  e.preventDefault();
+  $('.menu').toggleClass('menu_active');
+  $('.content').toggleClass('content_active');
 });
 
 var $btnTop = $('.btn-top')
 $(window).on("scroll", function(){
-  if ($(window).scrollTop() >= 900){
-    $btnTop.fadeIn();
-  }else {
-    $btnTop.fadeOut();
-  }
+if ($(window).scrollTop() >= 900){
+  $btnTop.fadeIn();
+}else {
+  $btnTop.fadeOut();
+}
 });
 $btnTop.on("click", function(){
 $("html,body").animate({scrollTop:0}, 1000);
 });
 
 function submitForm() {
-    // Получаем доступ к форме и её элементам по их id
-    var form = document.getElementById("myForm");
-    var selectaRole = document.getElementById("role");
-    var typeActicle = document.getElementById("type");
-    var titleActicle = document.getElementById("title");
-    var contentActicle = document.getElementById("content");
+  // Получаем доступ к форме и её элементам по их id
+  var selectaRole = document.getElementById("role");
+  var typeActicle = document.getElementById("type");
+  var titleActicle = document.getElementById("title");
+  var contentActicle = document.getElementById("content");
 
-    // Извлекаем значения из полей формы
-    var role = selectaRole.value;
-    var type = typeActicle.value;
-    var title = titleActicle.value;
-    var content = contentActicle.value;
-  
-    // Вызываем вашу функцию и передаем значения в неё
-    //processFormData(role, type, title, content);
+  // Извлекаем значения из полей формы
+  var role = selectaRole.value;
+  var type = typeActicle.value;
+  var title = titleActicle.value;
+  var content = contentActicle.value;
 
-    postRequest(role, type, title, content);
-  }
-  
-  function processFormData(role, type,title, content) {
-    // Ваш код для обработки данных из формы
-    console.log("user_role:", role);
-    console.log("post_type:", type);
-    console.log("post_title:", title);
-    console.log("post_body:", content);
-    // Здесь вы можете выполнить любую логику, связанную с данными из формы
-  }
+  // Вызываем вашу функцию и передаем значения в неё
+  processFormData(role, type, title, content);
+
+  postRequest(role, type, title, content);
+}
+
+function processFormData(role, type,title, content) {
+  // Ваш код для обработки данных из формы
+  console.log("user_role:", role);
+  console.log("post_type:", type);
+  console.log("post_title:", title);
+  console.log("post_body:", content);
+  // Здесь вы можете выполнить любую логику, связанную с данными из формы
+}
 async function postRequest(role, type, title, content)
 {
-    // Создайте объект FormData и добавьте данные в него
-    var formData = new FormData();
-    formData.append('user_role', role);
-    formData.append('post_type', type);
-    formData.append('post_title', title);
-    formData.append('post_body', content);
+  // Создайте объект FormData и добавьте данные в него
+  var formData = new FormData();
+  formData.append('user_role', role);
+  formData.append('post_type', type);
+  formData.append('post_title', title);
+  formData.append('post_body', content);
 
-    // Добавьте файл, если это необходимо
-    // formData.append('file', fileInput.files[0]);
+  // Добавьте файл, если это необходимо
+  // formData.append('file', fileInput.files[0]);
 
-    // Создайте объект для отправки запроса
-    var xhr = new XMLHttpRequest();
+  // Создайте объект для отправки запроса
+  var xhr = new XMLHttpRequest();
 
-    // Настройте запрос
-    xhr.open('POST', 'https://www.specposhiv.kiev.ua/api/posts', true);
+  // Настройте запрос
+  xhr.open('POST', 'https://www.specposhiv.kiev.ua/api/posts', true);
 
-    // Определите обработчик события для отслеживания состояния запроса
-    xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        // Запрос успешно выполнен, и получен ответ
-        console.log(xhr.responseText);
-    }
-    };
+  // Определите обработчик события для отслеживания состояния запроса
+  xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4 && xhr.status === 200) {
+      // Запрос успешно выполнен, и получен ответ
+      console.log(xhr.responseText);
+  }
+  };
 
-    // Отправьте объект FormData в запросе
-    xhr.send(formData);
+  // Отправьте объект FormData в запросе
+  xhr.send(formData);
+}
+function openFormAdd() 
+{
+  var form = document.querySelector('.popup-form-add');
+  form.style.display = 'block';
+}
+function closeFormAdd() 
+{
+  var form = document.querySelector('.popup-form-add');
+  form.style.display = 'none';
 }
--- GlobalBilgiInfo/lib/js/request-client.js
+++ GlobalBilgiInfo/lib/js/request-client.js
@@ -9,12 +9,15 @@ async function getPosts()
         document.querySelector('.accordion-shortcut').innerHTML += `
         <div class="accordion-item">
             <h2 class="accordion-header">
-                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-                    <b>${post.post_title}</b>
+                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">\
+                    <div class="wrap-content d-flex justify-content-space-between" style="width: 100%;">
+                        <div class="title-article-${post.id}"><b>${post.post_title}</b></div>
+                        <div class="feature_icons"><i onclick="getValue(${post.id})" class="fa-solid fa-pen-to-square"></i><i class="fa-solid fa-trash"></i></div>
+                    </div>
                 </button>
             </h2>
             <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show">
-                <div class="accordion-body"><strong>${post.post_body}</strong></div>
+                <div class="accordion-body content-article-${post.id}"><strong>${post.post_body}</strong></div>
             </div>
         </div>
         `
@@ -25,11 +28,14 @@ async function getPosts()
         <div class="accordion-item">
             <h2 class="accordion-header">
                 <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-                    <b>${post.post_title}</b>
+                    <div class="wrap-content d-flex justify-content-space-between" style="width: 100%;">
+                        <div class="title-article-${post.id}"><b>${post.post_title}</b></div>
+                        <div class="feature_icons"><i onclick="getValue(${post.id})" class="fa-solid fa-pen-to-square"></i><i class="fa-solid fa-trash"></i></div>
+                    </div>
                 </button>
             </h2>
             <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show">
-                <div class="accordion-body"><strong>${post.post_body}</strong></div>
+                <div class="accordion-body content-article-${post.id}"><strong>${post.post_body}</strong></div>
             </div>
         </div>
         `
@@ -40,11 +46,14 @@ async function getPosts()
         <div class="accordion-item">
             <h2 class="accordion-header">
                 <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-                    <b>${post.post_title}</b>
+                    <div class="wrap-content d-flex justify-content-space-between" style="width: 100%;">
+                        <div class="title-article-${post.id}"><b>${post.post_title}</b></div>
+                        <div class="feature_icons"><i onclick="getValue(${post.id})" class="fa-solid fa-pen-to-square"></i><i class="fa-solid fa-trash"></i></div>
+                    </div>
                 </button>
             </h2>
             <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show">
-                <div class="accordion-body"><strong>${post.post_body}</strong></div>
+                <div class="accordion-body content-article-${post.id}"><strong>${post.post_body}</strong></div>
             </div>
         </div>
         `
@@ -55,11 +64,14 @@ async function getPosts()
         <div class="accordion-item">
             <h2 class="accordion-header">
                 <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-                    <b>${post.post_title}</b>
+                    <div class="wrap-content d-flex justify-content-space-between" style="width: 100%;">
+                        <div class="title-article-${post.id}"><b>${post.post_title}</b></div>
+                        <div class="feature_icons"><i onclick="getValue(${post.id})" class="fa-solid fa-pen-to-square"></i><i class="fa-solid fa-trash"></i></div>
+                    </div>
                 </button>
             </h2>
             <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show">
-                <div class="accordion-body"><strong>${post.post_body}</strong></div>
+                <div class="accordion-body content-article-${post.id}"><strong>${post.post_body}</strong></div>
             </div>
         </div>
         `
@@ -68,3 +80,33 @@ async function getPosts()
 }
 getPosts();
 
+function getValue(id)
+{
+    var class_title = ".title-article-" + id;
+    var class_content = ".content-article-" + id;
+    var string = class_title;
+    var arr = string.split('-');
+    var article_id = arr[2];
+
+    var content_title = document.querySelector(class_title);
+    var content_body = document.querySelector(class_content);
+
+    if(content_title && content_body)
+    {
+        var input_value = document.getElementById('title-edit');
+        var body_value = document.getElementById('content-edit');
+        input_value.value = content_title.textContent;
+        body_value.value = content_body.textContent;
+        openForm();
+    }
+}
+function openForm() 
+{
+    var form = document.querySelector('.popup-form-edit');
+    form.style.display = 'block';
+}
+function closeForm() 
+{
+    var form = document.querySelector('.popup-form-edit');
+    form.style.display = 'none';
+}
